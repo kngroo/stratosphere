7e126b4cab8136832769fcef2b0a2e6d155a6134
Josh Kang <joshkang@gmail.com>
2023-09-06 15:00:28 +0900

    Add emoji support for md

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from "astro/config";
+import { rehypeAccessibleEmojis } from "rehype-accessible-emojis";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://kngroo.github.io",
   base: "/stratosphere",
+  markdown: {
+    rehypePlugins: [rehypeAccessibleEmojis],
+  },
 });
